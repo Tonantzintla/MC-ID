c13049ef12d2f5c25c6e88e77f463dad1b9372c6
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-09-06 22:10:59 +0200

    feat: some changes

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -7,10 +7,16 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://docs.mc-id.com",
   integrations: [
     starlight({
-      title: "My Docs",
-      social: [{ icon: "github", label: "GitHub", href: "https://github.com/withastro/starlight" }],
+      title: "MC-ID",
+      logo: {
+        dark: "./src/assets/MC-ID.svg",
+        light: "./src/assets/MC-ID-BG.svg",
+        replacesTitle: true
+      },
+      social: [{ icon: "github", label: "GitHub", href: "https://github.com/Tonantzintla/MC-ID" }],
       sidebar: [
         {
           label: "Guides",
@@ -23,7 +29,12 @@ export default defineConfig({
           label: "Reference",
           autogenerate: { directory: "reference" }
         }
-      ]
+      ],
+      editLink: {
+        baseUrl: "https://github.com/Tonantzintla/MC-ID/edit/master/docs/"
+      },
+      customCss: ["./src/styles/global.css"],
+      plugins: []
     }),
     sitemap(),
     svelte()
