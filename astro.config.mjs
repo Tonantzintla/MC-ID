3a1a69e592e14538577cca2e5f51be30fcf3716c
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-09-06 15:26:11 +0200

    Squashed 'docs/' content from commit f5cd88d
    git-subtree-dir: docs
    git-subtree-split: f5cd88d048360a02fdc0fb5d80cb2cd297386aa2

---
+++ astro.config.mjs
@@ -0,0 +1,35 @@
+// @ts-check
+import sitemap from "@astrojs/sitemap";
+import starlight from "@astrojs/starlight";
+import svelte from "@astrojs/svelte";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "My Docs",
+      social: [{ icon: "github", label: "GitHub", href: "https://github.com/withastro/starlight" }],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" }
+          ]
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" }
+        }
+      ]
+    }),
+    sitemap(),
+    svelte()
+  ],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
