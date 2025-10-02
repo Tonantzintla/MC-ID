96cac634e8ac8ccc6b8ba87638b067056e499d59
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-10-02 22:17:19 +0200

    fix: refactor DATABASE_URL handling for improved clarity and consistency

--- website/src/lib/server/db/index.ts
+++ website/src/lib/server/db/index.ts
@@ -3,9 +3,11 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-if (!env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
+const { DATABASE_URL } = env;
 
-const client = postgres(env.DATABASE_URL);
+if (!DATABASE_URL) throw new Error("DATABASE_URL is not set");
+
+const client = postgres(DATABASE_URL);
 
 export const db = drizzle(client, { schema });
 export type DB = typeof db;
